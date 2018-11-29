024f3b6e818fe5483b47f5f555571c2f4877b4f9
Chris Perlette <chrisperlette@gmail.com>
2018-11-28 22:33:16 -0700

    added tensor products

--- quantum.cc
+++ quantum.cc
@@ -11,6 +11,8 @@ struct complex{
 complex sum(complex,complex);
 complex product(complex,complex);
 complex conjugate(complex);
+vector<vector<complex> > tensorgen(vector<vector<complex> >,vector<vector<complex> >);
+vector<complex> tensor(vector<complex>,vector<complex>);
 double modulous(complex);
 void disp(complex);
 
@@ -81,4 +83,54 @@ complex conjugate(complex x)
 double modulous(complex x)
 {
 	return sqrt((pow(x.r,2)+pow(x.i,2)));
+}
+
+vector<vector<complex> > tensorgen(vector<vector<complex> > x,vector<vector<complex> > y)
+{
+	int i=0,j=0,k=0;
+	complex temp;
+	vector<complex> temp1;
+	vector<vector<complex> > thing;
+	for(i=0;i<((x[0]).size()*(y[0]).size());i++)
+	{
+		temp1.push_back(temp);
+	}
+	for(i=0;i<x.size()*y.size();i++)
+	{
+		thing.push_back(temp1);
+	}
+	for(i=0;i<thing.size();i++)
+	{
+		for(j=0;j<x[0].size();j++)
+		{
+			//*********************************
+			//Finish later if needed
+			//*********************************
+			for(k=0;k<y.size();k++)
+			{
+				//thing[i][j+k]=product();
+			}
+		}
+	}
+	return thing;
+}
+
+//Function specifically made for ket tensors
+vector<complex> tensor(vector<complex> x,vector<complex> y)
+{
+	int i=0,j=0;
+	complex temp1;
+	vector<complex> temp;
+	for(i=0;i<x.size()*y.size();i++)
+	{
+		temp.push_back(temp1);
+	}
+	for(i=0;i<x.size();i++)
+	{
+		for(j=0;j<y.size();j++)
+		{
+			temp[i+j]=product(x[i],y[j]);
+		}
+	}
+	return temp;
 }
