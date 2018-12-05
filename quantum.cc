5eb502c77597b89b268a034f7d753a30b897d8b0
Chris Perlette <chrisperlette@gmail.com>
2018-12-04 19:54:53 -0700

    just need to add some stuff to the end of D

--- quantum.cc
+++ quantum.cc
@@ -4,40 +4,51 @@
 
 using namespace std;
 
+//struct to represent a complex number
+//r = real value
+//i = imaginary coefficient
 struct complex{
 	double r,i;
 };
 
+//these are just really useful to have available
 complex zero,one;
 vector<complex> ketone,ketzero;
 
-void cnotinit();
+//Function prototypes
+void init();
+vector<complex> hadamard(vector<complex>);
+vector<complex> hadamardMagnum(vector<complex>);
+vector<complex> deutsch();
 vector<complex> toffoli(vector<complex>,vector<complex>,vector<complex>);
 vector<complex> cnot(vector<complex>,vector<complex>);
 complex sum(complex,complex);
 complex product(complex,complex);
+complex coproduct(complex,double);
 complex conjugate(complex);
 vector<vector<complex> > tensorgen(vector<vector<complex> >,vector<vector<complex> >);
 vector<complex> tensor(vector<complex>,vector<complex>);
 double modulous(complex);
 void disp(complex);
 
+
+//C-NOT arrays for use in C-NOT and Toffoli functions and the hadamard function
 complex fourbyfour[4][4];
 complex eightbyeight[8][8];
+complex hada[2][2];
 
 int main()
 {
-	cnotinit();
+	//initilize global variables and other useful stuff
+	init();
 	complex x,y;
 	vector<complex> a,b,test;
 	x.r=1;
 	x.i=1;
-	ketone.push_back(zero);
-	ketone.push_back(one);
-	ketzero.push_back(one);
-	ketzero.push_back(zero);
 	a=ketzero;
 	b=ketone;
+	/*
+	//This is just test stuff
 	cnot(ketzero,ketzero);
 	cout<<endl;
 	cnot(ketzero,ketone);
@@ -61,7 +72,8 @@ int main()
 	toffoli(b,b,a);
 	cout<<endl;
 	toffoli(b,b,b);
-	cout<<endl;
+	cout<<endl;*/
+	deutsch();
 	return 0;
 }
 
@@ -86,30 +98,42 @@ void disp(complex x)
 //Returns the sum
 complex sum(complex x,complex y)
 {
+	//why am I even bothering to comment this
 	complex temp;
 	temp.r=x.r+y.r;
 	temp.i=x.i+y.i;
 	return temp;
 }
 
-
+//returns the product of two complex numbers
 complex product(complex x,complex y)
 {
+	//multiply everything together
 	complex temp;
 	double a,b,c,d;
 	a=x.r*y.r;
 	b=x.r*y.i;
 	c=x.i*y.r;
 	d=(-1)*x.i*y.i;
+	//add that shiznit
 	temp.r=a+d;
 	temp.i=b+c;
+	//return to sender
 	return temp;
 }
 
+//returns product of a complex number and a coefficient
+complex coproduct(complex x, double y)
+{
+	x.r=x.r*y;
+	x.i=x.i*y;
+	return x;
+}
 //Function to return the conjugate of a complex number
 //Returns conjugate in complex form
 complex conjugate(complex x)
 {
+	//find and return the conjugate
 	complex temp;
 	temp.r=x.r;
 	temp.i=x.i*(-1);
@@ -120,27 +144,28 @@ complex conjugate(complex x)
 //Returns a double
 double modulous(complex x)
 {
+	//take a guess at what  this does
 	return sqrt((pow(x.r,2)+pow(x.i,2)));
 }
 
-//returns affected ket vector
+//returns full tensored vector for c-not, if you just want the affected ket change the return value to 'y' NOTE: this only works if you are using |0> and |1> 
 vector<complex> cnot(vector<complex> x,vector<complex> y)
 {
-	int xoff,yoff,decode;
+	//initilize stuff
+	int decode;
 	vector<complex> temp,ret;
+	//make the return value the right size
 	for(int i=0;i<4;i++){
 		ret.push_back(zero);
 	}
+	//get the tensor product of x and y
 	temp=tensor(x,y);
-	if(x[0].r==1){xoff=0;}
-	else{xoff=1;}
-	if(y[0].r==1){yoff=0;}
-	else{yoff=1;}
-	temp[(xoff*2)+yoff].r=1;
 	for(int i=0;i<4;i++)
 	{
+		//yep, that's a thing
 		ret[i]=sum(sum(product(fourbyfour[0][i],temp[0]),product(fourbyfour[1][i],temp[1])),sum(product(fourbyfour[2][i],temp[2]),product(fourbyfour[3][i],temp[3])));
 	}
+	//modify the affected ket and display results
 	for(int i=0;i<4;i++)
 	{
 		if(ret[i].r==1){decode=i;}
@@ -153,32 +178,30 @@ vector<complex> cnot(vector<complex> x,vector<complex> y)
 	else if(decode==2){
 		y=ketzero;
 	}
+	//return full vector
 	return ret;
 }
 
-//returns the affected qubit
+//Returns the full tenosr form for a toffoli gate, if you want it to return just the affected bit, change the return value to 'z' NOTE: this only works if you are using |0> and |1>
 vector<complex> toffoli(vector<complex> x,vector<complex> y,vector<complex> z)
 {
-	int xoff,yoff,zoff,decode;
+	//initilize temporary varibales
+	int decode;
 	vector<complex> temp, ret;
 	for(int i=0;i<8;i++)
 	{
 		ret.push_back(zero);
 	}
+	//create the tensor product for matrix multiplication
 	temp=tensor(x,y);
 	temp=tensor(temp,z);
-	if(x[0].r==1){xoff=0;}
-	else{xoff=1;}
-	if(y[0].r==1){yoff=0;}
-	else{yoff=1;}
-	if(z[0].r==1){zoff=0;}
-	else{zoff=1;}
-	temp[(xoff*4)+(yoff*2)+zoff]=one;
+	//matrix multiplication
 	for(int i=0;i<8;i++)
 	{
 		//trust me, it works
 		ret[i]=sum(sum(sum(product(eightbyeight[0][i],temp[0]),product(eightbyeight[1][i],temp[1])),sum(product(eightbyeight[2][i],temp[2]),product(eightbyeight[3][i],temp[3]))),sum(sum(product(eightbyeight[4][i],temp[4]),product(eightbyeight[5][i],temp[5])),sum(product(eightbyeight[6][i],temp[6]),product(eightbyeight[7][i],temp[7]))));
 	}
+	//modify affected ket and display results
 	for(int i=0;i<8;i++)
 	{
 		if(ret[i].r==1){decode=i;}
@@ -194,6 +217,48 @@ vector<complex> toffoli(vector<complex> x,vector<complex> y,vector<complex> z)
 	return ret;
 }
 
+//take a guess at what this does
+vector<complex> deutsch()
+{
+	complex uf[4][4];
+	for(int i=0;i<4;i++)
+	{
+		for(int j=0;j<4;j++)
+		{
+			uf[i][j]=zero;
+		}
+	}
+	double a,b,c,d;
+	vector<complex> temp,ret,top,bot;
+	complex x,y;
+	cout<<"Input Uf, line by line, in the format a b c d where a, b, c, and d are 1 or 0\n";
+	for(int i=0;i<4;i++)
+	{
+		cin>>a>>b>>c>>d;
+		uf[0][i].r=a;
+		uf[1][i].r=b;
+		uf[2][i].r=c;
+		uf[3][i].r=d;
+	}
+	top=ketzero;
+	bot=ketone;
+	top=hadamard(top);
+	bot=hadamard(bot);
+	temp=tensor(top,bot);
+	for(int i=0;i<4;i++)
+	{
+		ret.push_back(sum(sum(product(fourbyfour[0][i],temp[0]),product(fourbyfour[1][i],temp[1])),sum(product(fourbyfour[2][i],temp[2]),product(fourbyfour[3][i],temp[3]))));
+	}
+	ret=hadamard(ret);
+	for(int i=0;i<4;i++)
+	{
+		disp(ret[i]);
+		cout<<endl;
+	}
+	return ret;
+}
+
+//this does not work, but I also don't think i need to make it work
 vector<vector<complex> > tensorgen(vector<vector<complex> > x,vector<vector<complex> > y)
 {
 	int i=0,j=0,k=0;
@@ -224,32 +289,44 @@ vector<vector<complex> > tensorgen(vector<vector<complex> > x,vector<vector<comp
 	return thing;
 }
 
-//Function specifically made for ket tensors********Currently doesn't work
+//Function specifically made for 1-D vectors
 vector<complex> tensor(vector<complex> x,vector<complex> y)
 {
+	//initilize stuff
 	int i=0,j=0;
 	vector<complex> temp;
 	for(i=0;i<static_cast<int>(x.size())*static_cast<int>(y.size());i++)
 	{
+		//make temp the right size
 		temp.push_back(zero);
 	}
 	for(i=0;i<x.size();i++)
 	{
 		for(j=0;j<y.size();j++)
 		{
+			//assign values to the tensor product
 			temp[(i*2)+j]=product(x[i],y[j]);
 		}
 	}
+	//done, return tensor product
 	return temp;
 }
 
-void cnotinit()
+
+//this just initilizes all our varibles for use
+void init()
 {
+	//initilize generic complex values and vectors
 	zero.r=0;
 	zero.i=0;
 	one.r=1;
 	one.i=0;
+	ketone.push_back(zero);
+	ketone.push_back(one);
+	ketzero.push_back(one);
+	ketzero.push_back(zero);
 	int i,j;
+	//create 4X4 and 8X8 c-not matricies
 	for(i=0;i<4;i++)
 	{
 		for(j=0;j<4;j++)
@@ -272,4 +349,40 @@ void cnotinit()
 			}
 		}
 	}
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			if(j==1&&i==1)
+			{
+				hada[i][j]=coproduct(one,-1);
+			}
+			else
+			{
+				hada[i][j]=one;
+			}
+		}
+	}
+}
+
+vector<complex> hadamard(vector<complex> x)
+{
+	vector<complex> ret;
+	for(int i=0;i<2;i++)
+	{
+		ret.push_back(zero);
+	}
+	for(int i=0;i<2;i++)
+	{
+		ret[i]=sum(product(hada[0][i],x[0]),product(hada[1][i],x[1]));
+	}
+	ret[0]=coproduct(ret[0],1/sqrt(2));
+	ret[1]=coproduct(ret[1],1/sqrt(2));
+	return ret;
+}
+
+vector<complex> hadamardMagnum(vector<complex> x)
+{
+	complex neg=coproduct(one,-1);
+	complex hadamardXL[4][4]
 }
